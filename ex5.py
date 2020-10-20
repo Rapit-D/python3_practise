7b4443011c024cca56a8c6c4c911778c1c9bd7ea
root <root@ray.redhat8>
2020-10-20 23:03:21 +0800

    normal practise

---
+++ ex5.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+name = 'Xingyuan Ding'
+age = 28
+height = 175
+weight = 70
+eyes = 'black'
+teeth = 'white'
+hair = 'black'
+
+print(f"Let's talk about {name}.")
+print(f"He's {height} cm tall.")
+print(f"He's {weight} kg heavy.")
+print(f"Actually that's not too heavy.")
+print(f"He's got {eyes} eyes and {hair} hair.")
+print(f"His teeth are usually {teeth} depending on the coffe.")
+
+# tricky line. try it
+total = age + height + weight
+
+print(f"If I add {age}, {height}, and {weight} I get {total}.")
+print(round(1.7333))
