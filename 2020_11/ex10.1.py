fa6c297430773592727aa22f6ef979fe82c84f77
root <root@ray.redhat8>
2020-12-10 10:21:06 +0800

    编写倒计时脚本，遇到十位数转个位数时的输出错误

--- 2020_11/ex10.1.py
+++ 2020_11/ex10.1.py
@@ -1,8 +1,19 @@
 #!/usr/bin/python
 import time
 
-for i in '123456789':
+for i in range(9, -1, -1):
     print(f'\b{i}', end='', flush=True)
     time.sleep(1)
-    
-# has problem on this script
+print("")
+
+# 如果从10 以上的数字开始\b 退格一位，十位数会依旧显示。
+
+for i in range(10, -1, -1):
+    if abs(i) >= 9:
+        print(f'\b\b{i}', end='', flush=True)
+    elif abs(i) < 9:
+        print(f'\b{i}', end='', flush=True)
+    time.sleep(1)
+print("")
+
+# 还没有想明白10 到9 该怎么过渡
